767fa9ee563630702e62f1d52a16afd0eb277edf
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-17 08:20:14 +0000

    loaded all data to instance, tweaked model code, 50% dice score

--- ucsf_development/src/google_storage.py
+++ ucsf_development/src/google_storage.py
@@ -1,5 +1,6 @@
 # google storage functions
-from google.cloud import storage, language  # library = google-cloud-storage
+from google.cloud import storage, language  
+ # library = google-cloud-storage   # must be v1.17 or greater
  # library = google-cloud-language
 from io import BytesIO
 import pandas as pd
@@ -10,12 +11,16 @@ class Connect_Bucket:
     def __init__(self, project_id, bucket_name):
         self.project_id = project_id
         self.bucket_name = bucket_name
-        self.bucket = self._bucket_obj()
+        self.storage_client = None
+        if storage.__version__ >= '1.17.0':
+            self.bucket = self._bucket_obj()
+        else: 
+            print("Error: Please upgrade 'google-cloud-storage' package version to >= 1.17.0 ")
     
     def _bucket_obj(self):
         '''connect to google storage'''
-        storage_client = storage.Client(project = self.project_id)
-        bucket = storage_client.get_bucket(self.bucket_name)
+        self.storage_client = storage.Client(project = self.project_id)
+        bucket = self.storage_client.get_bucket(self.bucket_name)
         return bucket
     
     def list_blobs(self):
@@ -28,7 +33,16 @@ class Connect_Bucket:
         for blob in blobs:
             print(blob.name)
 
-
+    def get_file(self, file_path):
+        '''grab a blob from a bucket'''
+        print(self.bucket)
+        blob = storage.blob.Blob(file_path, self.bucket)
+        fileName = blob.name.split('/')[-1]
+        file_obj = BytesIO()
+        #blob.download_to_file(file_obj)
+        #return blob.download_to_file(file_obj, client=self.storage_client)
+        return blob.download_to_filename(filename)  
+            
     def upload_blob(source_file_name, destination_blob_name):
         """ Uploads a file to the bucket. """
         storage_client = storage.Client()
